5c71779e694f56ff0b970de208d17d5d3dd4ecf2
ansungwoo <asw4ever@naver.com>
2025-03-04 11:23:38 +0900

    UserController testCode 작성

---
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -0,0 +1,115 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserUpdateDto;
+import com.example.demo.repository.UserEntity;
+import com.example.demo.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+
+
+
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        String insertData = """
+            INSERT INTO users (id, email, nickname, address, certification_code, status, last_login_at)
+            VALUES (1, 'kok202@naver.com', 'kok202', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+
+            INSERT INTO users (id, email, nickname, address, certification_code, status, last_login_at)
+            VALUES (2, 'kok303@naver.com', 'kok303', 'Busan', 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'PENDING', 0);
+        """;
+        jdbcTemplate.execute(insertData);
+    }
+
+    @AfterEach
+    void tearDown() {
+        jdbcTemplate.execute("DELETE FROM users");
+    }
+    @Test
+    void 사용자는_특정_유저의_정보는_개인정보는_소거된_채_전달_받을_수_있다() throws Exception {
+        // when
+        mockMvc.perform(get("/api/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.email").value("kok202@naver.com"))
+                .andExpect(jsonPath("$.nickname").value("kok202"))
+                .andExpect(jsonPath("$.address").doesNotExist())
+                .andExpect(jsonPath("$.status").value("ACTIVE"));
+    }
+
+    @Test
+    void 사용자는_존재하지_않는_유저의_아이디로_api_호출할_경우_404_응답을_받는다() throws Exception {
+        // given
+
+        mockMvc.perform(get("/api/users/123456789"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Users에서 ID 123456789를 찾을 수 없습니다."));
+    }
+
+    @Test
+    void 사용자는_인증_코드로_계정을_활성화_시킬_수_있다() throws Exception {
+        // given
+
+        mockMvc.perform(get("/api/users/2/verify")
+                        .queryParam("certificationCode","bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"))
+                .andExpect(status().isFound());
+
+        UserEntity userEntity = userRepository.findById(2L).get();
+        assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
+
+    }
+    @Test
+    void 사용자는_내_정보를_수정할_수_있다() throws Exception {
+        // given
+        UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+                .nickname("kok202-n")
+                .address("강남구")
+                .build();
+
+        // when & then
+        mockMvc.perform(put("/api/users/me")
+                        .header("EMAIL", "kok202@naver.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userUpdateDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))  // expectedValue는 실제 값으로 변경 필요
+                .andExpect(jsonPath("$.email").value("kok202@naver.com"))
+                .andExpect(jsonPath("$.nickname").value("kok202-n"))
+                .andExpect(jsonPath("$.address").value("강남구"))
+                .andExpect(jsonPath("$.status").value("ACTIVE"));
+    }
+
+
+}
